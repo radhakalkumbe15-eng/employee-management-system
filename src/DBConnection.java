238311594ef42cfd04c90073b69dff459eb3190e
Radha Sukhdeo Kaklkumbe <radhakalkumbe15@gmail.com>
2026-09-21 16:19:19 +0530

    Update database password to a placeholder

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 public class DBConnection {
     private static final String URL="jdbc:mysql://localhost:3306/employee_management";
     private static final String USER="root";
-    private static final String PASSWORD="MYSQL@1234";
+    private static final String PASSWORD="YOUR_PASSWORD";
     public static Connection getConnection() throws SQLException{
         return 
         DriverManager.getConnection(URL,USER,PASSWORD);
